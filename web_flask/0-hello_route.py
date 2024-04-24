1dc804732e84bf15e2f79d14a8001c488ea68cf9
Mbelenga <mbelengaterence@gmail.com>
2024-04-24 17:16:54 +0300

    Starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 def hello_hbnb():
     """
     Routing to root, the purpose of strict_slashes makes sure
-    both versions of the URL (with and without trailing slashes) for the same endpoint
+    both versions of the URL (with and without trailing slashes)
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
