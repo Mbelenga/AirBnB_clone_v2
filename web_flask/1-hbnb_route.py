62520ed119e8b2642e9581340bbd7b5d0b21816e
Mbelenga <mbelengaterence@gmail.com>
2024-04-24 17:51:35 +0300

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A script that starts the flask Web Application """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Routing to root """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    The purpose of strict_slashes makes sure
+    both versions of the URL (with and without trailing slashes)
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+
